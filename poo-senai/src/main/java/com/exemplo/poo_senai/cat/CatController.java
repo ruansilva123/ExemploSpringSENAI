b75cad4ca97532c6f570d01997581f9314e399cf
RuanCarlosSilva <143547755+ruansilva123@users.noreply.github.com>
2024-11-21 16:08:27 -0300

    :sparkles: feat: finalizando exemplo

--- poo-senai/src/main/java/com/exemplo/poo_senai/cat/CatController.java
+++ poo-senai/src/main/java/com/exemplo/poo_senai/cat/CatController.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
@@ -20,6 +24,7 @@ public class CatController {
 
     public CatController(){
         cats.add(new Cat("Felino", 5));
+        cats.add(new Cat("Bola de pelo", 3));
     }
 
     @GetMapping
@@ -37,5 +42,31 @@ public class CatController {
             return new ResponseEntity<>(cat.get(), HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createCat(@RequestBody Cat cat){
+        cats.add(cat);
+        return new ResponseEntity<>("Cat created successfully!", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{name}")
+    public ResponseEntity<String> updateCat(@PathVariable String name, @RequestBody Cat cat){
+        for (int i = 0; i < cats.size(); i++){
+            if (cats.get(i).getName().equals(name)){
+                cats.set(i, cat);
+                return new ResponseEntity<>("Cat updated successfully!", HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>("Cat not found!", HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("{/{name}}")
+    public ResponseEntity<String> deleteCat(@PathVariable String name){
+        boolean removed = cats.removeIf(cat -> cat.getName().equals(name));
+        if (removed){
+            return new ResponseEntity<>("Cat deleted successfully!", HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>("Cat not found!", HttpStatus.NOT_FOUND);
     }
 }
