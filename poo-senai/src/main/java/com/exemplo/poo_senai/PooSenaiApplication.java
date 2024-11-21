0eaba64d15a0ffe6ec2446dad446a6531d17dd27
RuanCarlosSilva <143547755+ruansilva123@users.noreply.github.com>
2024-11-21 14:40:13 -0300

    :sparkles: feat: first commit

---
+++ poo-senai/src/main/java/com/exemplo/poo_senai/PooSenaiApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.poo_senai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooSenaiApplication.class, args);
+	}
+
+}
---
+++ poo-senai/src/test/java/com/exemplo/poo_senai/PooSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.poo_senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
